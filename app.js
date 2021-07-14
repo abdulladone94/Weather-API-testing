4a996749f9b51902fd8accdcf9f57ca5a886e9ff
abdulladone94 <abdulladone94@gmail.com>
2021-07-14 21:44:34 +0530

    Converting data into js object

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const https = require('https');
 const app = express()
 
 app.get('/', (req, res) => {
-   res.send('Server is up and running...')
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=colombo&appid=d7ef3ce0eecd0cbc9fc900c9b6417f90'
+    https.get(url, (response) => {
+        // console.log(response.statusCode);
+
+        response.on('data', (data) => {
+            const weather = (JSON.parse(data));
+            console.log(weather);
+            
+        })
+    })
 })
 
 app.listen(3001, () => {console.log("Started running port 3001");})
